f544c5c6049f0d9b6322fc56f264340752f22053
knoxaramav2 <wtscott@iastate.edu>
2020-06-21 21:38:51 -0500

    token defs

--- Src/Libs/Tokenizer/knxToken.cpp
+++ Src/Libs/Tokenizer/knxToken.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 using namespace KNX_TOKENIZER;
 
-KLANG_TOKEN::KLANG_TOKEN(string term){
-
+KLANG_TOKEN::KLANG_TOKEN(string term, KnxTokenType type){
+    this->term = term;
+    this->type = type;
 };
--- Src/Libs/Tokenizer/knxToken.hpp
+++ Src/Libs/Tokenizer/knxToken.hpp
@@ -2,14 +2,16 @@
 
 #include <string>
 #include "../debug/debugInfo.hpp"
+#include "knxTokenType.hpp"
 
 namespace KNX_TOKENIZER{
-struct KLANG_TOKEN : KLANG_DEBUG_LINE{
-    KLANG_TOKEN(string term);
-    std::string term;
+    struct KLANG_TOKEN : KLANG_DEBUG_LINE{
+        KLANG_TOKEN(string term, KnxTokenType type);
+        std::string term;
+        KnxTokenType type;
 
-    private:
-};
+        private:
+    };
 
 }
 
